bc7f0a0c260f9ec3709ceaa3c0e9fdac669597e1
Daniel <danielochoa@outlook.com>
2020-10-11 18:14:45 -0500

    first commit

---
+++ server/server.ts
@@ -0,0 +1,42 @@
+
+
+import * as express from 'express';
+import {Application} from "express";
+import {getAllCourses, getCourseById} from "./get-courses.route";
+import {searchLessons} from "./search-lessons.route";
+import {loginUser} from "./auth.route";
+import {saveCourse} from "./save-course.route";
+
+const bodyParser = require('body-parser');
+
+const cors = require('cors');
+
+
+const app: Application = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(cors({ origin: true, credentials: true }));
+
+
+app.route('/api/login').post(loginUser);
+
+app.route('/api/courses').get(getAllCourses);
+
+app.route('/api/courses/:id').put(saveCourse);
+
+app.route('/api/courses/:id').get(getCourseById);
+
+app.route('/api/lessons').get(searchLessons);
+
+
+
+
+const httpServer = app.listen(9000, () => {
+    console.log("HTTP REST API Server running at http://localhost:" + httpServer.address().port);
+});
+
+
+
+
